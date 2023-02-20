93dc65fab33ac2ba369e5f313d29a252481417df
GabbLps <jhosegabriel95@gmail.com>
2023-02-20 00:22:15 -0300

    first commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * isso é um comentário <b>javadoc</b>
+ */
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("HA-ME-HA-ME-HA");
+}
+}
